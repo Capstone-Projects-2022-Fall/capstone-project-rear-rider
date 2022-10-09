c3ee61e2e087deb69e988fdda04148b9d3fe8885
Brock Morris <brock36morris@gmail.com>
2022-10-08 23:15:49 -0400

    I changed the library to PiCamera2 and added some code to get fram rate and file size. the videos save to the directory that the module is located in an no longer the cwd. PI CAMERA 2

--- camera_capture.py
+++ camera_capture.py
@@ -1,6 +1,9 @@
-from picamera import PiCamera
+from picamera2 import Picamera2, Preview
+from time import sleep
 from datetime import datetime
 from datetime import date
+import os
+from videoprops import get_video_properties
 
 now = datetime.now()
 today = date.today()
@@ -9,15 +12,21 @@ current_date = str(today.strftime('%Y-%m-%d'))
 print("Current Time = " + current_time)
 print("Current Date = " + current_date)
 
-photo_name = "image at " + current_time + " on "+ current_date +".jpg"
-vid_name = "vid at " + current_time + " on "+ current_date + ".h264"
+runtime_path = os.path.dirname(__file__) + "/"
 
-camera = PiCamera()
-camera.start_preview()
-camera.capture(photo_name)
-camera.stop_preview()
-camera.start_preview()
-camera.start_recording(vid_name)
-camera.wait_recording(5)
-camera.stop_recording()
-camera.stop_preview()
+photo_name = runtime_path + "image at "    + current_time + " on " + current_date + ".jpg"
+vid_name = runtime_path   + "orig vid at " + current_time + " on " + current_date + ".h264"
+
+
+picam2 = Picamera2() 
+#camera_config = picam2.create_preview_configuration() 
+#picam2.configure(camera_config) 
+picam2.start() 
+sleep(2) 
+picam2.capture_file(photo_name)
+props = get_video_properties(photo_name)
+print(photo_name + ">>>>>>>>>>>" + str(props['width']) + "x" + str(props["height"]))
+
+picam2.start_and_record_video(vid_name, duration = 10)
+props = get_video_properties(vid_name)
+print(photo_name + ">>>>>>>>>>>" + str(props['width']) + "x" + str(props["height"]) + "\n frames: " + str(props["avg_frame_rate"]))
