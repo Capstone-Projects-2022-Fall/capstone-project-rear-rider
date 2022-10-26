3b67666c0d0cead632c6df9efa22787df3387af2
Matthew O'Mara <tuh39725@temple.edu>
2022-10-26 04:54:07 -0400

    format distance output

--- rear_rider_device/rear_rider_sensors/lidar.py
+++ rear_rider_device/rear_rider_sensors/lidar.py
@@ -18,7 +18,7 @@ def getTFminiData():
                 distance = recv[2] + recv[3] * 256
                 strength = recv[4] + recv[5] * 256
                 # print('(', distance, ',', strength, ')')
-                print("Distatnce: %f Signal Strength: %d"%((distance * 0.01), strength))
+                print("Distatnce: %3.2fm Signal Strength: %d"%((distance * 0.01), strength))
                 ser.reset_input_buffer()
                 
             if recv[0] == 'Y' and recv[1] == 'Y':     #python2
