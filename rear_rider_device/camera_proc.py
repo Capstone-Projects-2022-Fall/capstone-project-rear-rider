6ace5e8a94f6468ba67d173cdd8b7c82a8d47d73
Brock Morris <brock36morris@gmail.com>
2022-10-26 15:15:27 -0400

    I have added a way for the camera process to interact with the whole parent child system. I can integrate it further with the main and the threading but this is a good save point.

---
+++ rear_rider_device/camera_proc.py
@@ -0,0 +1,41 @@
+import asyncio
+from pkgutil import get_data
+import readline
+from sys import stdout
+from ipc.parent_process import ParentProcess
+import rear_rider_sensors.camera as camera
+import os
+from datetime import datetime
+from datetime import date
+
+class CameraParentProcess(ParentProcess):
+    def __init__(self, camera: camera.RRCamera):
+        self.camera = camera
+
+    async def pre_ready(self):
+        pass
+    def pre_done(self):
+        pass
+    def no_ack(self):
+        pass
+    def no_on_handler(self):
+        pass
+
+    async def on_get_picture(self, name = str(datetime.now().strftime("%H:%M:%S")) + " on " + str(date.today().strftime('%Y-%m-%d'))):
+        self.camera.takePhoto(name + ".jpg")
+        #await self.writeline('on_picture')
+    async def on_get_stream(self):
+        self.camera.beginStream()
+    async def on_get_recording(self):
+        self.camera.startRec("IPC_RECORDING_TEST.mp4", 15)
+    async def on_get_end_stream(self):
+        self.camera.endStream()
+    async def on_help(self):
+        self.writeline(">get_picture: takes a photo via the camera and saves it.\n\t-no parameters needed, but you can specify a name if needed.\n" + 
+                       ">get_stream: starts up the stream and which can be monitored on 'localhost:8000'. \n\t-no parameters neeeded\n" + 
+                       ">get_recording: starts a recording that lasts for a 10 seconds.\n\t-no parameters needed, but you can specify a name if needed.\n")
+if __name__ == '__main__':
+    cam = camera.RRCamera()
+    camera_parent_proc = CameraParentProcess(camera=cam)
+    #Begin session with the parent process. The parent process is collecting the data.
+    asyncio.run(camera_parent_proc.begin())
--- rear_rider_device/main.py
+++ rear_rider_device/main.py
@@ -3,16 +3,18 @@ import concurrent.futures
 from ipc.i_process import Process
 from accelerometer_child_proc import AccelerometerChildProcess
 from bluetooth_server_child_proc import BluetoothServerChildProcess
+from camera_child_proc import CameraChildProcess
 
 import os 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def main():
     bt_server_process = BluetoothServerChildProcess()
-    accelerometer_proc = AccelerometerChildProcess(buf_size=32, fps=1,
-        bt_server_proc=bt_server_process)
+    camera_proc = CameraChildProcess()
+    #accelerometer_proc = AccelerometerChildProcess(buf_size=32, fps=1,
+    #    bt_server_proc=bt_server_process)
     child_processes: list[Process] = [
-        accelerometer_proc,
+        #accelerometer_proc,
         bt_server_process,
     ]
     futures = []
