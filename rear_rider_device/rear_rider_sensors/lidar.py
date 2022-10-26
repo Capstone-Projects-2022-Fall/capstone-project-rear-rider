5c5bf781b9f678b0577190577f8fda620ffd58e3
Matthew O'Mara <tuh39725@temple.edu>
2022-10-26 04:51:04 -0400

    refactor

--- rear_rider_device/rear_rider_sensors/lidar.py
+++ rear_rider_device/rear_rider_sensors/lidar.py
@@ -17,7 +17,8 @@ def getTFminiData():
             if recv[0] == 0x59 and recv[1] == 0x59:     #python3
                 distance = recv[2] + recv[3] * 256
                 strength = recv[4] + recv[5] * 256
-                print('(', distance, ',', strength, ')')
+                # print('(', distance, ',', strength, ')')
+                print("Distatnce: %f Signal Strength: %d"%((distance * 0.01), strength))
                 ser.reset_input_buffer()
                 
             if recv[0] == 'Y' and recv[1] == 'Y':     #python2
@@ -27,8 +28,7 @@ def getTFminiData():
                 highS = int(recv[5].encode('hex'), 16)
                 distance = lowD + highD * 256
                 strength = lowS + highS * 256
-                print("Distatnce: %f Signal Strength: %d"%((distance * 0.01), strength))
-                # time.sleep(1)
+                print(distance, strength)
             
             # you can also distinguish python2 and python3: 
             #import sys
