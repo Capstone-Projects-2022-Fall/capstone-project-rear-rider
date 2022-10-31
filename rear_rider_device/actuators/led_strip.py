6d2afed5737261aa59c4a2b29c57a838fe6eeb53
jon-dez <48642222+jon-dez@users.noreply.github.com>
2022-10-31 01:06:25 -0400

    Added concept of 'led effects' so that they can be applied in layers.

--- rear_rider_device/actuators/led_strip.py
+++ rear_rider_device/actuators/led_strip.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import time
 import board
 import neopixel
@@ -7,6 +8,14 @@ INIT_BRIGHTNESS = 0.008
 
 OFF_COLOR = (0,0,0)
 WHITE = (255,255,255)
+
+DEFAULT_FPS = 5
+"""
+10 frames per second is good for a strobe light with 5 blinks per second.
+"""
+
+MAX_BRIGHTNESS = 0.1
+
 
 class LedStripController():
     def __init__(self, pixels: neopixel.NeoPixel):
@@ -73,39 +82,104 @@ class LedStripController():
     
     def set_brightness(self, value: float):
         """
-        0.0 - 1.0
+        0.0 - 1.0, but is limited to MAX_BRIGHTNESS which scales the brightness additionaly from 0.0 - 1.0
         """
-        self._pixels.brightness = self._brightness_value = value
+        self._pixels.brightness = self._brightness_value = value * MAX_BRIGHTNESS
         self._pixels.show()
     
     def show(self):
         self._pixels.show()
+    
+    def set(self, index: int, color: tuple[int, int, int]):
+        self._pixels[index] = color
         
 
 def create_neopixel():
     return neopixel.NeoPixel(GPIO_PIN, NUM_PIXELS, brightness=INIT_BRIGHTNESS)
+    
+
+@dataclass
+class LedStripFrame():
+    """
+    A from of the led strip at one point in time.
+    """
+    fps: int
+    frame_num: int = 0
+    start_time: int = 0
+    end_time: int = 0
+    time_elapsed: int = 0
+
+class LedStripEffect():
+    """
+    Using
+    """
+    def affect(self, frame: LedStripFrame, led_strip_con: LedStripController):
+        """
+        An inheriting class can control what the effect looks like at the given frame.
+        This should happen as fast as possible as it is akin to generating a signal.
+        """
+        raise NotImplementedError('LightStripEffect.show() is not implemented yet!')
+
+    def new_frame(self, frame: LedStripFrame):
+        """
+        An inheriting class should do any calculations here for the `affect` phase.
+        """
+        raise NotImplementedError('')
+
+class StrobeEffect(LedStripEffect):
+    """
+    Blinks at a frequency matching the frame's fps.
+    """
+    def affect(self, frame: LedStripFrame, led_strip_con: LedStripController):
+        led_strip.fill(self._color)
+        
+    def new_frame(self, frame: LedStripFrame):
+        if frame.frame_num % 2 == 0:
+            self._color = WHITE
+        else:
+            self._color = OFF_COLOR
+
+
+def enter_leds_effects_loop(effects: list[LedStripEffect], fps):
+    frame = LedStripFrame(fps)
+    def effects_too_slow():
+        # print('!!! The effects are too slow for the framerate !!!')
+        pass
+    def update_elapsed_time():
+        frame.time_elapsed = frame.end_time - frame.start_time
+    frame_duration = 1.0/frame.fps
+    frame.start_time = time.time_ns()
+    while True:
+        for effect in effects:
+            update_elapsed_time()
+            effect.new_frame(frame)
+        for effect in effects:
+            update_elapsed_time()
+            effect.affect(frame, led_strip_con=led_strip)
+        frame.end_time = time.time_ns()
+        update_elapsed_time()
+        time_to_sleep = frame_duration - frame.time_elapsed / 1_000_000_000.0
+        if time_to_sleep < 0:
+            effects_too_slow()
+            continue
+        time.sleep(time_to_sleep)
+        led_strip.show()
+        frame.start_time = time.time_ns()
+        frame.frame_num += 1
+        # print('slept for {} second(s).'.format(time_to_sleep))
 
 
 if __name__ == '__main__':
     pixels = create_neopixel()
     led_strip = LedStripController(pixels)
+    # TODO add an Effect that selects leds in a group to apply an effect to. Effect mask?
     try:
-        while True:
-            # led_strip.test_rgb()
-            # led_strip.strobe(2, 16)
-            led_strip._pixels.brightness = 0.1
-            led_strip._pixels[0] = (255, 0, 0)
-            led_strip._show_then_delay(1)
-            led_strip._pixels[1] = (0, 255, 0)
-            led_strip._show_then_delay(1)
-            led_strip._pixels[2] = (0, 0, 255)
-            led_strip._show_then_delay(1)
-            led_strip._pixels[0] = (0, 0, 0)
-            led_strip._show_then_delay(1)
-            led_strip.fill((0,0,0))
-            led_strip._show_then_delay(1)
-            # led_strip.turn_on()
-    except KeyboardInterrupt:
-        pass
-    led_strip.turn_off()
-    pixels.deinit()
+        effects: list[LedStripEffect] = [
+            StrobeEffect()
+        ]
+        enter_leds_effects_loop(effects=effects, fps=DEFAULT_FPS)
+    except Exception as e:
+        print('turning off')
+        led_strip.turn_off()
+        pixels.deinit()
+
