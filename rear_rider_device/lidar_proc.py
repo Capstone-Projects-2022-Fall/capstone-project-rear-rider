c1006b3390f7e3e7e17d40a63d5b3608d67152e5
Matthew O'Mara <tuh39725@temple.edu>
2022-11-04 11:29:07 -0400

    add lidar parrent proc still needs child proc

---
+++ rear_rider_device/lidar_proc.py
@@ -0,0 +1,33 @@
+import asyncio
+from pkgutil import get_data
+import readline
+from sys import stdout
+from ipc.parent_process import ParentProcess
+import rear_rider_sensors.lidar as lidar
+
+class LidarParentProcess(ParentProcess):
+    def __init__(self, lidar: lidar.Lidar):
+        self.lidar = lidar
+
+    async def pre_ready(self):
+        pass
+
+    def pre_done(self):
+        pass
+    
+    def no_ack(self):
+        pass
+    
+    def no_on_handler(self):
+        pass
+
+    async def on_get_data(self):
+        lidar = self.lidar
+        data = ("data\n %f %d"%lidar.getTFminiData() + "\n")
+        self.writeline(data)
+
+if __name__ == '__main__':
+    lidar = lidar.Lidar()
+    lidar_parent_proc = LidarParentProcess(lidar=lidar)
+    # Begin session with the parent process. The parent process is collecting the data.
+    asyncio.run(lidar_parent_proc.begin())
--- rear_rider_device/rear_rider_sensors/lidar.py
+++ rear_rider_device/rear_rider_sensors/lidar.py
@@ -4,43 +4,48 @@ import time
 
 ser = serial.Serial("/dev/ttyS0", 115200)
 
-def getTFminiData():
-    while True:
-        #time.sleep(0.1)
-        count = ser.in_waiting  # Count the number of bytes in the input buffer
-        if count > 8:
-            recv = ser.read(9)   # Read up to 9 bytes
-            ser.reset_input_buffer() # Clear the buffer
-            # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
-            # type(recv[0]), 'str' in python2, 'int' in python3 
-            
-            if recv[0] == 0x59 and recv[1] == 0x59:     #python3
-                distance = recv[2] + recv[3] * 256
-                strength = recv[4] + recv[5] * 256
-                # print('(', distance, ',', strength, ')')
-                print("Distatnce: %3.2fm Signal Strength: %d"%((distance * 0.01), strength))
-                ser.reset_input_buffer()
+class Lidar():
+    def __init__(self) -> None:
+        pass
+
+    def getTFminiData():
+        while True:
+            #time.sleep(0.1)
+            count = ser.in_waiting  # Count the number of bytes in the input buffer
+            if count > 8:
+                recv = ser.read(9)   # Read up to 9 bytes
+                ser.reset_input_buffer() # Clear the buffer
+                # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
+                # type(recv[0]), 'str' in python2, 'int' in python3 
                 
-            if recv[0] == 'Y' and recv[1] == 'Y':     #python2
-                lowD = int(recv[2].encode('hex'), 16)      
-                highD = int(recv[3].encode('hex'), 16)
-                lowS = int(recv[4].encode('hex'), 16)      
-                highS = int(recv[5].encode('hex'), 16)
-                distance = lowD + highD * 256
-                strength = lowS + highS * 256
-                print(distance, strength)
-            
-            # you can also distinguish python2 and python3: 
-            #import sys
-            #sys.version[0] == '2'    #True, python2
-            #sys.version[0] == '3'    #True, python3
+                if recv[0] == 0x59 and recv[1] == 0x59:     #python3
+                    distance = recv[2] + recv[3] * 256
+                    strength = recv[4] + recv[5] * 256
+                    # print('(', distance, ',', strength, ')')
+                    print("Distatnce: %3.2fm Signal Strength: %d"%((distance * 0.01), strength))
+                    ser.reset_input_buffer()
+                    
+                if recv[0] == 'Y' and recv[1] == 'Y':     #python2
+                    lowD = int(recv[2].encode('hex'), 16)      
+                    highD = int(recv[3].encode('hex'), 16)
+                    lowS = int(recv[4].encode('hex'), 16)      
+                    highS = int(recv[5].encode('hex'), 16)
+                    distance = lowD + highD * 256
+                    strength = lowS + highS * 256
+                    print(distance, strength)
+                
+                # you can also distinguish python2 and python3: 
+                #import sys
+                #sys.version[0] == '2'    #True, python2
+                #sys.version[0] == '3'    #True, python3
 
 
 if __name__ == '__main__':
+    lidar = Lidar()
     try:
         if ser.is_open == False:
             ser.open()
-        getTFminiData()
+        lidar.getTFminiData()
     except KeyboardInterrupt:   # Ctrl+C
         if ser != None:
             ser.close()
