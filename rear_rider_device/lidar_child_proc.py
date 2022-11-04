818fee7de7ec4c2bafc389f4685017a9bbec3b4f
Matthew O'Mara <tuh39725@temple.edu>
2022-11-04 17:34:12 -0400

    read sensor from child process

--- rear_rider_device/lidar_child_proc.py
+++ rear_rider_device/lidar_child_proc.py
@@ -9,15 +9,12 @@ import os
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 class LidarChildProcess(ChildProcess):
-    def __init__(self, bt_server_proc: BluetoothServerChildProcess, buf_size: int = 64, fps: int = 60):
+    def __init__(self):
         """
         Default `buf_size` of 64 frame datapoints at 60 `fps`.
         """
         super().__init__('python {}/lidar_proc.py'.format(dir_path))
-        self.cyclic_buff = Deque[tuple[float, float]](maxlen=buf_size)
-        self.fps = fps
         self.ready = asyncio.Future()
-        self.bt_server_proc = bt_server_proc
     
     async def on_ready(self):
         self.start_time = datetime.now()
@@ -32,19 +29,20 @@ class LidarChildProcess(ChildProcess):
         self._print('after_on_ready')
 
     async def on_data(self):
-        self._print('on_data {}/{} fps: {} start: {} current: {}'.format(
-            len(self.cyclic_buff), 
-            self.cyclic_buff.maxlen,
-            self.fps,
-            self.start_time.strftime('%x %X.%f'),
-            datetime.now().strftime('%x %X.%f'),
-        ))
+        # self._print('on_data {}/{} fps: {} start: {} current: {}'.format(
+        #     len(self.cyclic_buff), 
+        #     self.cyclic_buff.maxlen,
+        #     self.fps,
+        #     self.start_time.strftime('%x %X.%f'),
+        #     datetime.now().strftime('%x %X.%f'),
+        # ))
+
         lidar_data = await self.readline()
         self._print(
             '{}\n'
             '\t{}\n'.format(
                 lidar_data[0],
-                lidar_data[1],
+                lidar_data[1]
             )
         )
 
@@ -53,10 +51,11 @@ class LidarChildProcess(ChildProcess):
             float((lidar_data[0]* 0.01)),
             float(lidar_data[1]),
         )
-        self.cyclic_buff.append(data)
-        await self.bt_server_proc.writeline(
-            'set_data\n'
-            'lidar\n'
-            '{},{}'.format(data[0], data[1]))
-        await asyncio.sleep(1.0/self.fps)
+        
+        # Needs to send led notification
+        # await self.bt_server_proc.writeline(
+        #     'set_data\n'
+        #     'lidar\n'
+        #     '{},{}'.format(data[0], data[1]))
+        await asyncio.sleep(1.0/100)
         await self.writeline('get_data')
