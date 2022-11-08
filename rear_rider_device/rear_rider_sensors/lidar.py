a8d7d2bf6d62357bd6e1c7042d52da806841f568
Matthew O'Mara <tuh39725@temple.edu>
2022-11-07 23:02:58 -0500

    remove python2 comments

--- rear_rider_device/rear_rider_sensors/lidar.py
+++ rear_rider_device/rear_rider_sensors/lidar.py
@@ -16,31 +16,14 @@ class Lidar():
             if count > 8:
                 recv = ser.read(9)   # Read up to 9 bytes
                 ser.reset_input_buffer() # Clear the buffer
-                # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
-                # type(recv[0]), 'str' in python2, 'int' in python3 
+                # type(recv[0]),  'int' in python3 
                 
                 if recv[0] == 0x59 and recv[1] == 0x59:     #python3
                     distance = recv[2] + recv[3] * 256
                     strength = recv[4] + recv[5] * 256
-                    # print('(', distance, ',', strength, ')')
                     # print("Distatnce: %3.2fm Signal Strength: %d"%((distance * 0.01), strength))
                     ser.reset_input_buffer()
                     return distance, strength
-                    
-                # if recv[0] == 'Y' and recv[1] == 'Y':     #python2
-                #     lowD = int(recv[2].encode('hex'), 16)      
-                #     highD = int(recv[3].encode('hex'), 16)
-                #     lowS = int(recv[4].encode('hex'), 16)      
-                #     highS = int(recv[5].encode('hex'), 16)
-                #     distance = lowD + highD * 256
-                #     strength = lowS + highS * 256
-                #     print(distance, strength)
-                
-                # you can also distinguish python2 and python3: 
-                #import sys
-                #sys.version[0] == '2'    #True, python2
-                #sys.version[0] == '3'    #True, python3
-
 
 if __name__ == '__main__':
     lidar = Lidar()
