99ce87108f347a3567a656f309a69cbbdd15a85c
Matthew O'Mara <tuh39725@temple.edu>
2022-11-14 04:04:16 -0500

    change import statment

--- rear_rider_device/camera_proc.py
+++ rear_rider_device/camera_proc.py
@@ -2,7 +2,7 @@ import asyncio
 from pkgutil import get_data
 import readline
 from sys import stdout
-from ipc.parent_process import ParentProcess
+from rear_rider_device.ipc.parent_process import ParentProcess
 import rear_rider_device.rear_rider_sensors.camera as camera
 import sys
 import os
