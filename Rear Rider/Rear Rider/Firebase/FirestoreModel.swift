e999bff8198f08715a984eb456e59b8737d5d429
rpsutton <57606697+rpsutton@users.noreply.github.com>
2022-11-15 19:03:50 -0500

    creating MetricsModel and adding writeRide() function

--- Rear Rider/Rear Rider/Firebase/FirestoreModel.swift
+++ Rear Rider/Rear Rider/Firebase/FirestoreModel.swift
@@ -10,15 +10,39 @@ import Firebase
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
+@MainActor
 class FirestoreModel: ObservableObject {
     
     var db = Firestore.firestore()
+    var userId = Auth.auth().currentUser?.uid
+    
+    @Published var appError: AppError? = nil
+    @Published var firestoreLoading = false
     
     struct AppError: Identifiable {
         let id = UUID().uuidString
         let errorString: String
     }
     
-    @Published var appError: AppError? = nil
+    enum Result {
+        case success
+        case failure
+    }
     
+    struct FirestoreResult {
+        var id = UUID()
+        var res: Result
+        var message: String?
+    }
+    
+    func writeRide(ride: Ride) -> FirestoreResult {
+        firestoreLoading = true
+        do {
+            try db.collection("users").document(userId!).collection("rides").document().setData(from: ride)
+            return FirestoreResult(res: .success, message: "Succesfully wrote ride to db")
+        } catch {
+            return FirestoreResult(res: .failure, message: error.localizedDescription)
+        }
+    }
+
 }
