a94732216ed3bbc1d77df93b731602ff0ad4ec3b
jon-dez <48642222+jon-dez@users.noreply.github.com>
2022-11-23 13:02:37 -0500

    Various modifications: - Remove some whitespace - Add a docstring to the class. - Convert string formatting to f-format variant.

--- rear_rider_device/bluetooth_server_child_proc.py
+++ rear_rider_device/bluetooth_server_child_proc.py
@@ -1,23 +1,21 @@
-from rear_rider_device.ipc.child_process import ChildProcess
-
 import os
-
+from rear_rider_device.ipc.child_process import ChildProcess
 from rear_rider_device.leds_child_proc import LedsChildProcess
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-
-
 class BluetoothServerChildProcess(ChildProcess):
+    '''
+    This class handles the messages received from the bluetooth server child process.
+    '''
     def __init__(self, leds_child_process: LedsChildProcess):
-        super().__init__("python {}/bluetooth.py".format(dir_path))
+        super().__init__(f'python {dir_path}/bluetooth.py')
         self._leds_child_process = leds_child_process
-    
+
     def _get_name(self) -> str:
         return 'BluetoothServerChildProcess'
-    
 
     def _print_header(self, message: str):
-        return super()._print('==== Bluetooth Process ====\n{}'.format(message))
+        return super()._print(f'==== Bluetooth Process ====\n{message}')
 
     async def on_wait_ready(self):
         while True:
@@ -33,9 +31,8 @@ class BluetoothServerChildProcess(ChildProcess):
 
     async def on_sensor_data_stream_ready(self):
         self._print_header('sensor_data_stream begin')
-        
         self._print_header('sensor_data_stream_end')
-    
+
     async def on_set_data_ack(self):
         self._print('on_set_data_ack')
 
@@ -61,30 +58,28 @@ class BluetoothServerChildProcess(ChildProcess):
         """
         self._print('on_led_strobe_off')
         await self._leds_child_process.led_strobe_off()
-    
+
     async def is_strobe_on(self):
         self._print('is_strobe_on')
-    
+
     async def is_strobe_on_response(self, strobe_on: bool):
         await self.writeline(
             'strobe_on\n'
-            '{}'.format(strobe_on))
-        # await self._wait_ack('strobe_on')
-    
+            f'{strobe_on}')
+
     async def on_no_on_handler(self):
         line = await self.readline()
-        self._print('on no on handler: {}'.format(line))
+        self._print(f'on no on handler: {line}')
 
     async def on_help(self):
         while True:
             line = await self.readline()
             if line == '== help_string_end ==':
                 break
-        pass
 
     def no_on_handler(self, on_command, err):
-        self._print('no on handler s: {}\n,{}'.format(on_command, err))
-    
+        self._print(f'no on handler s: {on_command}\n,{err}')
+
     async def on_discoverable(self):
         """
         The handler for when bluetooth changes its discoverability to other devices.
@@ -96,7 +91,7 @@ class BluetoothServerChildProcess(ChildProcess):
             await self._leds_child_process.set_discoverable_effect(True)
         elif discoverable == '0':
             await self._leds_child_process.set_discoverable_effect(False)
-    
+
     async def on_led_config(self):
         line = await self.readline()
         self._print(f'led_config: {line}')
